022e2700df4f734c62c018c0b5485a1d5f3927ce
felipearaujo97 <89814271+felipearaujo97@users.noreply.github.com>
2021-10-18 20:18:58 -0300

    Add files via upload

---
+++ Python/ex01.py
@@ -0,0 +1,9 @@
+#Entrar via teclado com a base e a altura de um retângulo, calcular e exibir sua área.
+
+b = float(input("Digite a Base: "))
+al = float(input("Digite a Altura: "))
+ 
+a = (b * al) 
+
+print(f"A area do retângulo é: {a:.2f}")
+
---
+++ Python/ex02.py
@@ -0,0 +1,7 @@
+#Calcular e exibir a área de um quadrado, a partir do valor de sua aresta(lado) que será digitado.
+
+lado = float(input("Digite o valor da aresta: "))
+
+a = lado * lado
+
+print(f"A area do quadrado é: {a:.2f}")
