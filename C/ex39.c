96f65be1bc505f6b56fb49b7f4ad64ed849c78ae
felipearaujo97 <89814271+felipearaujo97@users.noreply.github.com>
2021-11-07 22:44:52 -0300

    Add files via upload

---
+++ C/ex39.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include<stdio.h>
+#include<time.h> 
+#include<string.h>
+#include<locale.h>  
+
+int main(int argc, char *argv[]){
+  //int numeros[2][2] = {4, 5, 7, 10};
+  int numeros[3][4]; int i; int j; int mult;
+ 
+  printf("Digite os numeros para matriz 3 x 4 \n");
+  for(i=0; i<3; i++)
+  {
+      for(j=0; j<4; j++){
+        scanf("%i", &numeros[i][j]);
+      }
+  }
+
+  	printf("\n\nDigite o fator multiplicativo:");
+	scanf("%i", &mult);
+ 
+  printf("Os numeros digitados na matriz foram: \n");
+  for(i=0; i<3; i++){
+      for(j=0; j<4; j++){
+      	numeros[i] [j]= numeros [i] [j]*mult;
+        printf("%i ", numeros[i][j]);
+      }
+      printf("\n");
+  }
+ 
+  return 0;
+}
+
